ef0f9fab387971e4b4f1a28120320b22097ba206
jp-mena <jpmena@miuandes.cl>
2024-03-26 16:21:23 -0300

    Este es el commit final del laboratorio 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,13 @@ class StaticPagesController < ApplicationController
     end 
     def contact
     end
+    def contact_submit
+        # Procesar los datos del formulario
+        # Por ejemplo, puedes usar params para acceder a los datos enviados desde el formulario
+        # y luego realizar acciones como guardarlos en la base de datos, enviar un correo electrónico, etc.
+    
+        # Después de procesar los datos, puedes redirigir a la misma página de contacto con un mensaje de agradecimiento
+        flash[:success] = "Thank you for your message, we will contact you soon."
+        redirect_to contact_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  post '/contact_submit', to: 'static_pages#contact_submit'
 end
